969ce02666e95607cc2b5cf34a2cde23d770831b
Lien3001 <kh.brink03@outlook.com>
2023-08-05 14:51:37 +0200

    Create directory for web app

---
+++ cs50/project/app.py
@@ -0,0 +1,30 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+import datetime
+from project.functions import login_required
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+
+@app.route("/")
+@login_required
+def index():
+
+
+    return render_template()
+
+
